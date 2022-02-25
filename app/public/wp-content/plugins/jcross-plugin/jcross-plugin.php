9764ea1243cf4020cffedf7e7eb2d62b206ee818
joshacross <joshuaalexandercross@gmail.com>
2022-02-25 00:53:10 -0600

    added PHP a local file/php

--- app/public/wp-content/plugins/jcross-plugin/jcross-plugin.php
+++ app/public/wp-content/plugins/jcross-plugin/jcross-plugin.php
@@ -2,6 +2,7 @@
 /**
  * @package jcrossPlugin
  */
+
 /*
 Plugin Name: Jcross Plugin
 Plugin URI: http://jcross.com/plugin
@@ -12,3 +13,44 @@ Author URI: https://joshacross.github.com/my-profile
 License: GPLv2 or later
 Text Domain: jcross-plugin
  */
+
+/*
+This program is free software; you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more <details class="
+You should have received a copy of the GNU General publice license along with this program; if not,
+write to the Free Software Foundtion, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+*/
+
+defined ( 'ABSPATH ') or die ( 'Hrmm... It does not seem that you have access to this file. Please contact system administrator');
+
+class JcrossPlugin {
+
+    __construct($string) {
+        echo $string;
+    }
+
+    function method1() {
+
+    }
+
+    function method2() {
+
+    }
+}
+
+
+if ( class_exists( 'JcrossPlugin' ) ) {
+    $jcrossPlugin = JcrossPlugin( 'Jcross Plugin Initialized' );
+}
+
+
+function customFunction ($args) {
+    echo $arg;
+}
+
+customFunction('this is my argument to echo...' );
