c9d6e6987b444f3b4c8727bcfa9b7bae5b51f33c
joshacross <joshuaalexandercross@gmail.com>
2022-02-27 22:55:11 -0600

    added json namefiles

---
+++ app/public/wp-content/plugins/jcrossing-plugin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ecd91123eb53bb9bc7b8fcefa087ef8dfc64af6b',
+        'name' => 'joshuacross/plugin-test',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joshuacross/plugin-test' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ecd91123eb53bb9bc7b8fcefa087ef8dfc64af6b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
