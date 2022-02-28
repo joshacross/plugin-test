da3f004e6323d653cc3fd6726c9443d9d7539d0c
joshacross <joshuaalexandercross@gmail.com>
2022-02-27 22:07:29 -0600

    created admin page for plugin with settings reference link

--- app/public/wp-content/plugins/jcrossing-plugin/jcrossing-plugin.php
+++ app/public/wp-content/plugins/jcrossing-plugin/jcrossing-plugin.php
@@ -34,12 +34,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class JcrossingPlugin 
 {
+    public $plugin;
+    
     function __construct() {
-        add_action( 'init', array ( $this, 'custom_post_type' ) );
+        $this->plugin = plugin_basename( __FILE__ );
     }
 
     function register() {
         add_action( 'admin_enqueue_scripts', array ( $this, 'enqueue' ) );
+
+        add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+        add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
+    }
+
+    function settings_link( $links ) {
+        // add custom settings link
+        $settings_link = '<a href="admin.php?page=jcrossing_plugin">Settings</a>';
+        array_push( $links, $settings_link );
+        return $links;
+    }
+
+    function add_admin_pages() {
+        add_menu_page ( 'Jcrossing Plugin', 'Jcross', 'manage_options', 'jcrossing_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+    }
+
+    function admin_index() {
+        require_once plugin_dir_path( __FILE__ ) . '../jcrossing-plugin/templates/admin.php';
+    }
+
+    protected function create_post_type() {
+        add_action( 'init', array ( $this, 'custom_post_type' ) );
     }
 
     function activate () {
---
+++ app/public/wp-content/plugins/jcrossing-plugin/templates/admin.php
@@ -0,0 +1,2 @@
+<h1>Jcrossing Plugin </h1>
+
